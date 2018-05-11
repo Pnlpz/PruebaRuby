d985c91d9264d3c7c7485e908415254dfcee7c81
Marisol Smith <marisolsmith@gmail.com>
2018-05-11 20:16:42 -0300

    array

--- pruebaruby.rb
+++ pruebaruby.rb
@@ -1,10 +1,4 @@
-# Se tiene un archivo con formato .csv que contiene nombres de alumnos con sus
-# respectivas notas.
-# El archivo tiene la siguiente estructura:
-# David, 9, 2, 5, 1, 1
-# Gonzalo, 10, 2, A, 8, 1
-# Mai, 10, 10, 9, 9, A
-# JP, 10, 10, 10, 10, 10
+
 # Donde la A señala que el alumno estuvo ausente en la prueba.
 # Crear un menú con 4 opciones:
 # Se debe validar, en caso que se ingrese otra opción, que la opción
@@ -12,39 +6,71 @@
 # programa debe mostrar que la opción es inválida, mostrar nuevamente
 # el menú y la posibilidad de volver a ingresar una opción
 
+file = File.open('alumnos.csv', 'r')
+alumnos = file.readlines
+file.close
+# print alumnos
 
-notas = { 'David' => [9, 2, 5, 1, 1], 'Gonzalo' => [10, 2, 'A', 8, 1],
-          'Mai' => [10, 10, 9, 9, 'A'], 'JP' => [10, 10, 10, 10, 10] }
+data = alumnos.map { |notas| notas.split(', ') }
+print data
+
+
+def promedio(x)
+  suma = 0
+  data[x].map do |valor|
+    suma += valor[x].to_i
+  end
+  print suma / 5
+end
+
+promedio(3)
+
+# alumnos.each do |notas|
+#   notas.split(', ')
+#   print notas
+# end
+
+# alumnos.each_with_index do |line, i|
+#   if line % 6 == 0
+#     puts line
+# end
+
+# suma = 0
+# alumnos.map do |notas|
+#   suma += notas[0].to_i
+# end
+# print suma
 
 def menu
   loop do
+    opcion = 0
     print 'Ingresa una opción: '
-    puts '1.- Promedios'
+    puts '1.- Promedios estudiantes'
     puts '2.- Inasistencias'
-    puts '3.- Aprobados'
+    puts '3.- Estudiantes aprobados'
     puts '4.- Salir'
     opcion = gets.chomp.to_i
     case opcion
       when 1
+
         #Debe generar un archivo con el nombre de cada alumno y el
         # promedio de sus notas.
         # Se puede leer el archivo completo o ir leyendo y procesando
         # por línea, ambas opciones son válidas.
-
       when 2
         #Debe contar la cantidad de inasistencias totales y mostrarlas en
         # pantalla.
-
       when 3
         #Debe mostrar los nombres de los alumnos aprobados. Para eso
         # se debe crear un método que reciba -como argumento- la nota necesaria pa
         # aprobar, por defecto esa nota debe ser 5.
       when 4
+        exit
         # Debe terminar el programa.
-
       else
         puts "opción inválida"
       end
     end
+end
 
-  end
+menu
---
+++ pruebarubyf.rb
@@ -0,0 +1,6 @@
+# def parse_notas(alumno, nota1, nota2, nota3, nota4, nota5)
+# end
+
+file = File.open('alumnos.csv', 'r')
+alumnos = file.readlines
+file.close
