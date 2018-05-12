8f4a66447bc4e55dc1b719efa863694747953b4a
Marisol Smith <marisolsmith@gmail.com>
2018-05-12 00:12:47 -0300

    promedios

--- pruebaruby.rb
+++ pruebaruby.rb
@@ -1,45 +1,32 @@
-
-# Donde la A señala que el alumno estuvo ausente en la prueba.
-# Crear un menú con 4 opciones:
-# Se debe validar, en caso que se ingrese otra opción, que la opción
-# escogida sea 1, 2, 3, o 4. Por ende, si se ingresa cualquier otra opción el
-# programa debe mostrar que la opción es inválida, mostrar nuevamente
-# el menú y la posibilidad de volver a ingresar una opción
-
 file = File.open('alumnos.csv', 'r')
 alumnos = file.readlines
 file.close
-# print alumnos
+#print alumnos
 
 data = alumnos.map { |notas| notas.split(', ') }
 print data
 
+#promedios
+david = data[0].inject(0) { |sum, i| sum + i.to_i}
+puts david / 5.0
+gonzalo = data[1].inject(0) { |sum, i| sum + i.to_i}
+puts gonzalo / 5.0
+mai = data[2].inject(0) { |sum, i| sum + i.to_i}
+puts mai / 5.0
+jp = data[3].inject(0) { |sum, i| sum + i.to_i}
+puts jp / 5.0
 
-def promedio(x)
-  suma = 0
-  data[x].map do |valor|
-    suma += valor[x].to_i
-  end
-  print suma / 5
-end
 
-promedio(3)
 
-# alumnos.each do |notas|
-#   notas.split(', ')
-#   print notas
-# end
 
-# alumnos.each_with_index do |line, i|
-#   if line % 6 == 0
-#     puts line
-# end
 
+# f = File.new('promedios.txt', 'w')
+# promedios =
 # suma = 0
-# alumnos.map do |notas|
-#   suma += notas[0].to_i
+# data[0].each do |valores|
+#   suma += valores.to_i
+#   print suma
 # end
-# print suma
 
 def menu
   loop do
@@ -52,7 +39,13 @@ def menu
     opcion = gets.chomp.to_i
     case opcion
       when 1
-
+        file = File.open("promedios.txt", "w") do |file|
+        file.puts "David tiene promedio #{david}"
+        file.puts "Gonzalo tiene promedio #{gonzalo}"
+        file.puts "Mai tiene promedio #{mai}"
+        file.puts "Jp tiene promedio #{jp}"
+        end
+        file.close
         #Debe generar un archivo con el nombre de cada alumno y el
         # promedio de sus notas.
         # Se puede leer el archivo completo o ir leyendo y procesando
@@ -73,4 +66,4 @@ def menu
     end
 end
 
-menu
+#menu
