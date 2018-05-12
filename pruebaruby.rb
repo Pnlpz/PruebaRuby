39ea310ad638634417a0358723ecb91911823fc5
Marisol Smith <marisolsmith@gmail.com>
2018-05-12 11:23:41 -0300

    mmm

--- pruebaruby.rb
+++ pruebaruby.rb
@@ -1,17 +1,23 @@
 file = File.open('alumnos.csv', 'r')
-alumnos = file.readlines
+alumnos = file.readlines.map(&:chomp)
 file.close
-#print alumnos
+# print alumnos
 
 data = alumnos.map { |notas| notas.split(', ') }
 print data
 
-#promedios
-david = data[0].inject(0) { |sum, i| sum + i.to_i}
-gonzalo = data[1].inject(0) { |sum, i| sum + i.to_i}
-mai = data[2].inject(0) { |sum, i| sum + i.to_i}
-jp = data[3].inject(0) { |sum, i| sum + i.to_i}
+# Inasistencias // no puedo hacer lo mismo dentro del menú. me dice que la
+# variable data no está definida
+inasistencia = data.collect.count { |x| x.include?('A') }
+print "En total hay #{inasistencia} inasistencia(s)"
 
+# Promedios
+david = data[0].inject(0) { |sum, i| sum + i.to_i }
+gonzalo = data[1].inject(0) { |sum, i| sum + i.to_i }
+mai = data[2].inject(0) { |sum, i| sum + i.to_i }
+jp = data[3].inject(0) { |sum, i| sum + i.to_i }
+
+# Archivo nuevo con promedios
 file = File.open('promedios.txt', 'w')
 file.puts "David tiene promedio #{david / 5.0}"
 file.puts "Gonzalo tiene promedio #{gonzalo / 5.0}"
@@ -19,13 +25,23 @@ file.puts "Mai tiene promedio #{mai / 5.0}"
 file.puts "Jp tiene promedio #{jp / 5.0}"
 file.close
 
-# f = File.new('promedios.txt', 'w')
-# promedios =
-# suma = 0
-# data[0].each do |valores|
-#   suma += valores.to_i
-#   print suma
+# Aprobados
+print "David aprueba con promedio #{david / 5.0}" if david / 5.0 >= 5
+print "Gonzalo aprueba con promedio #{gonzalo / 5.0}" if gonzalo / 5.0 >= 5
+print "Mai aprueba con promedio #{mai / 5.0}" if mai / 5.0 >= 5
+print "JP aprueba con promedio #{jp / 5.0}" if jp / 5.0 >= 5
+
+# aprobados
+# def aprobados(i)
+#   i = gets.chomp
+#   if i >= 5
+#     puts "#{i} aprueba"
+#   else
+#     print "#{i} reprueba"
 # end
+# end
+
+aprobados(5)
 
 def menu
   loop do
@@ -42,24 +58,20 @@ def menu
         promedios = file.read
         file.close
         print promedios
-        #Debe generar un archivo con el nombre de cada alumno y el
-        # promedio de sus notas.
-        # Se puede leer el archivo completo o ir leyendo y procesando
-        # por línea, ambas opciones son válidas.
       when 2
-        #Debe contar la cantidad de inasistencias totales y mostrarlas en
+        print (Data.collect.count { |x| x.include?('A') })
+        # Debe contar la cantidad de inasistencias totales y mostrarlas en
         # pantalla.
       when 3
-        #Debe mostrar los nombres de los alumnos aprobados. Para eso
+        # Debe mostrar los nombres de los alumnos aprobados. Para eso
         # se debe crear un método que reciba -como argumento- la nota necesaria pa
         # aprobar, por defecto esa nota debe ser 5.
       when 4
         exit
-        # Debe terminar el programa.
       else
         puts "opción inválida"
       end
     end
 end
 
-menu
+#menu
